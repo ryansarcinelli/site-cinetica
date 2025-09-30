4460c3524b1a69b9d18c1767f35a3edb728303c9
Lukas Basilio <lukas.r.basilio@gmail.com>
2025-09-29 23:29:37 -0300

    Criação da página de Contato e do Componente header
    Criei a página de contato com a empresa que esta faltando apenas o ajustes de alguns detalhes do layout e a logica do enviou com o uso de uma API para o email. Também fiz o componente header que seja utilizado em todas as páginas do site.

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from '../views/homeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/contato',
+    name: 'contato',
+    component: () => import('../views/contatoView.vue') 
+  },
+  
+  //{
+  //  path: '/feedback',
+  //  name: 'feedback',
+  //  component: () => import('../views/FeedbackView.vue')
+ // },
+ // {
+  //  path: '/sobre',
+   // name: 'sobre',
+   // component: () => import('../views/SobreView.vue')
+  //},
+ // {
+   // path: '/servicos',
+    //name: 'servicos',
+    //component: () => import('../views/ServicosView.vue')
+  //},
+  //{
+    //path: '/portfolio',
+    //name: 'portfolio',
+    //component: () => import('../views/PortfolioView.vue')
+  //}
+];
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+});
+
+export default router;
---
+++ src/views/homeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <AppHeader /> 
+  
+  <main>
+    <h1>PAGINA INICIAL AAAAAAA</h1>
+    </main>
+</template>
+
+<script>
+
+import AppHeader from '../components/header.vue';
+
+export default {
+  name: 'App',
+ 
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #F6E8CD;
+}
+
+main {
+    padding: 20px;
+    text-align: center;
+}
+</style>
